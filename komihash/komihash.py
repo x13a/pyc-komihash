aa65b7c64eaaafbd2b83bb069f6dbcbc311ac785
lucky <>
2021-12-05 16:07:36 +0300

    fix komirand
    change fn names

--- komihash/komihash.py
+++ komihash/komihash.py
@@ -3,13 +3,20 @@ from cython.cimports import ckomihash
 
 @cython.ccall
 @cython.exceptval(0, check=True)
-def komihash(key: bytes, seed: cython.ulonglong = 0) -> cython.ulonglong:
+def hash64(key: bytes, seed: cython.ulonglong = 0) -> cython.ulonglong:
     return ckomihash.komihash(cython.cast(cython.p_char, key), len(key), seed)
 
-@cython.ccall
-@cython.exceptval(0, check=True)
-def komirand(seed1: cython.ulonglong, seed2: cython.ulonglong) -> cython.ulonglong:
-    return ckomihash.komirand(
-        cython.cast(cython.p_ulonglong, cython.address(seed1)), 
-        cython.cast(cython.p_ulonglong, cython.address(seed2)),
-    )
+@cython.cclass
+class Rand:
+
+    _seed1: cython.ulonglong
+    _seed2: cython.ulonglong
+
+    def __cinit__(self, seed1, seed2):
+        self._seed1 = seed1
+        self._seed2 = seed2
+    
+    @cython.ccall
+    @cython.exceptval(0, check=True)
+    def rand(self) -> cython.ulonglong:
+        return ckomihash.komirand(cython.address(self._seed1), cython.address(self._seed2))
--- test.py
+++ test.py
@@ -3,8 +3,12 @@ import komihash
 
 class KomihashTestCase(unittest.TestCase):
     def test_komihash(self):
-        self.assertEqual(komihash.komihash(b'test1'), 5270236647356506095)
-        self.assertEqual(komihash.komihash(b'test2'), 11456916202224026228)
+        self.assertEqual(komihash.hash64(b'test1'), 5270236647356506095)
+        self.assertEqual(komihash.hash64(b'test2'), 11456916202224026228)
     
     def test_komirand(self):
-        self.assertEqual(komihash.komirand(0, 0), 12297829382473034410)
+        rng = komihash.Rand(0, 0)
+        self.assertEqual(rng.rand(), 12297829382473034410)
+        self.assertEqual(rng.rand(), 18446744073709551614)
+        self.assertEqual(rng.rand(), 5270498306774157584)
+        self.assertEqual(rng.rand(), 13469051228422846976)
