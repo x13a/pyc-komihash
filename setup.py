3080a3fe1e6061c705872d475346fd1fd5b6190e
lucky <>
2021-12-26 02:43:39 +0300

    update to version 4.1
    sync version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 
 setup(
     name='komihash',
-    version='0.3.6.1',
+    version='4.1',
     ext_modules=cythonize([Extension('komihash', ['komihash/komihash.py'])], language_level=3),
     zip_safe=False,
 )
